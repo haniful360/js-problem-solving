dda9667dcc1dddfb9a543cf2ad3b230ccfcb9ff9
haniful360 <haniful360@gmail.com>
2023-07-27 23:31:32 +0600

    calculator problem solving added

--- problem-solving.js
+++ problem-solving.js
@@ -126,6 +126,35 @@ function generateRandomPassword(length) {
   
   const passwordLength = 12;
   const randomPassword = generateRandomPassword(passwordLength);
-  console.log(randomPassword);
+  console.log('randomPassword :',randomPassword);
+
+
+// task 7 : Implement a simple JavaScript calculator. The calculator should take two numbers and an operator (+, -, *, /) as input and return the result of the operation.
+  function calculator(num1, operator, num2) {
+    switch (operator) {
+      case '+':
+        return num1 + num2;
+      case '-':
+        return num1 - num2;
+      case '*':
+        return num1 * num2;
+      case '/':
+        if (num2 !== 0) {
+          return num1 / num2;
+        } else {
+          return "Error: Division by zero is not allowed!";
+        }
+      default:
+        return "Error: Invalid operator!";
+    }
+  }
+  
+
+  const number1 = 10;
+  const number2 = 5;
+  const operation = '*';
+  const output5 = calculator(number1, operation, number2);
+  console.log(output5);
+  
   
   
